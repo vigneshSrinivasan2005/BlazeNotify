af04c651163c06f708a94a682515da2ed0aa7164
vigneshSrinivasan2005 <vignesh.2005.10.10@gmail.com>
2023-10-07 00:15:49 -0700

    got the number data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url="https://firms.modaps.eosdis.nasa.gov/api/area/csv/efc81a030842f1f2809a40ade2162752/LANDSAT_NRT/world/1";
+const url="https://firms.modaps.eosdis.nasa.gov/api/area/csv/efc81a030842f1f2809a40ade2162752/VIIRS_SNPP_NRT/world/1";
 const https = require('https');
 var data2={};
 https.get(url, (resp) => {
@@ -7,17 +7,21 @@ https.get(url, (resp) => {
   // A chunk of data has been received.
   resp.on('data', (chunk) => {
     data += chunk;
+    //console.log(chunk);
   });
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    console.log(data);
-    data2=data;
+    console.log(typeof(data));
+    start(data);
+    //console.log(data.JSON);
+
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
-data2.array.forEach(element => {
-    console.log(element);
-});
+function start(data){
+    console.log(data);
+
+}
